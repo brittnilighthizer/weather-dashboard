2a90e00c5f0c396d5013b932b9aecbe2e9695529
Brittni Lighthizer <brittnikirsten@gmail.com>
2020-03-16 08:49:50 -0500

    improved functionality and rendering, need to add some more front end styling

--- script.js
+++ script.js
@@ -1,94 +1,106 @@
-var runningHistory = []
+var granimInstance = new Granim({
+  element: '#canvas-basic',
+  direction: 'radial',
+  isPausedWhenNotInView: true,
+  states: {
+    "default-state": {
+      gradients: [
+        ['#ff9966', '#ff5e62'],
+        ['#00F260', '#0575E6'],
+        ['#e1eec3', '#f05053']
+      ]
+    }
+  }
+});
+
+var runningHistory = window.localStorage.getItem("#history") || []
 render();
 $("#search").on("click", function () {
-$(".city-country ").empty();
-$(".current-temp").empty();
-$(".humidity ").empty();
-$(".windspeed ").empty();
-$(".uv-index ").empty();
-$(".five-day ").empty();
-$("#error").empty();
+  $(".city-country ").empty();
+  $(".current-temp").empty();
+  $(".humidity ").empty();
+  $(".windspeed ").empty();
+  $(".uv-index ").empty();
+  $(".fiveDay ").empty();
+  $("#error").empty();
 
-var APIKey = "cf033af614b15264df2b80a9a4d6be21";
-var userCity = $("#searchbar").val();
-console.log(userCity)
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userCity + "&appid=" + APIKey;
-
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-console.log(response)
-  let results = [response.name, response.wind.speed, response.wind.deg, response.main.humidity, response.main.temp,]
-
-  $(".city-country").append("In " + (response.name) + ", " + (response.sys.country) + "... ");
-  $(".current-temp").append("...the current temperature is " + Math.floor((response.main.temp - 273.15) * 1.80 + 32) + " &deg;F");
-  $(".humidity").append("...the humidity level is " + (response.main.humidity) + " %");
-  $(".windspeed").append("...the wind speed is " + response.wind.speed + " MPH");
-
-  var lat = response.coord.lat;
-  var lon = response.coord.lon;
-  var uvQuery = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + lat + "&lon=" + lon;
+  var APIKey = "cf033af614b15264df2b80a9a4d6be21";
+  var userCity = $("#searchbar").val();
+  console.log(userCity)
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userCity + "&appid=" + APIKey;
 
   $.ajax({
-    url: uvQuery,
+    url: queryURL,
     method: "GET"
-  }).then(function(response) {
-    var uvIndex = response.value;
+  }).then(function (response) {
+    console.log(response)
+    let results = [response.name, response.wind.speed, response.wind.deg, response.main.humidity, response.main.temp,]
+
+    $(".city-country").append("In " + (response.name) + ", " + (response.sys.country) + "... ");
+    $(".current-temp").append("...the current temperature is " + Math.floor((response.main.temp - 273.15) * 1.80 + 32) + " &deg;F");
+    $(".humidity").append("...the humidity level is " + (response.main.humidity) + " %");
+    $(".windspeed").append("...the wind speed is " + response.wind.speed + " MPH");
+
+    var lat = response.coord.lat;
+    var lon = response.coord.lon;
+    var uvQuery = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + lat + "&lon=" + lon;
+
+    $.ajax({
+      url: uvQuery,
+      method: "GET"
+    }).then(function (response) {
+      var uvIndex = response.value;
       $(".uv-index").append("...the UV Index is " + uvIndex + ".");
 
-  });
+    });
 
-  var fiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?id=" + response.id + "&appid=" + APIKey;
+    var fiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?id=" + response.id + "&appid=" + APIKey;
 
-  $.ajax({
-    url: fiveDayUrl,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response)
-    let addedDays = 1
-    for (let i = 0; i < 40; i+=9) {
-      let day = moment().add(addedDays, 'days').format('MM/DD/YYYY')
-      addedDays++;
-      //add the moment variable above to below jquery object
-      let fiveDayForecast = $(`<div class="fiveDay">
+    $.ajax({
+      url: fiveDayUrl,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response)
+      let addedDays = 1
+      for (let i = 0; i < 40; i += 9) {
+        let day = moment().add(addedDays, 'days').format('MM/DD/YYYY')
+        addedDays++;
+        //add the moment variable above to below jquery object
+        let fiveDayForecast = $(`<div class='tile is-child box is-pulled-right'>
                               <p><b>${day}</b></p> 
                               <img src=https://openweathermap.org/img/wn/${response.list[i].weather[0].icon}.png>
-                              <p><b>Temp</b>:${Math.floor(((response.list[i].main.temp - 273.15) * 1.8) + 32)} degrees</p>
+                              <p>${Math.floor(((response.list[i].main.temp - 273.15) * 1.8) + 32)} \u00B0</p>
                               <p><b>Humidity</b>:${response.list[i].main.humidity}%</p>
-                              </div>`)
+                              </div></div>`)
 
-      $(".five-day").append(fiveDayForecast);
+        $(".fiveDay").append(fiveDayForecast);
+      }
 
-    }
-    
-    // adjust styling for this to be seen properly
-    runningHistory.push(userCity);
-    
-    window.localStorage.setItem("#history",JSON.stringify(runningHistory))
-    
-    
-    // console.log(localStorageCity);
-    $(".history").append(userCity);
-    
-    
+      // adjust styling for this to be seen properly
 
-});
+      if (runningHistory.includes(userCity)) {
+        render();
 
-// fix so this is not always displaying
-})
-.catch(function(response) {
-  $("#error").empty();
-  console.log("failed")
-  $("#error").append(userCity + "is not a valid city.");
-})
-  
-  
+      } else {
+        runningHistory.push(userCity);
+        window.localStorage.setItem("#history", JSON.stringify(runningHistory))
+        render();
+      }
+
+    });
+  })
+    .catch(function (response) {
+      $("#error").empty();
+      console.log("failed")
+      $("#error").append(userCity + "is not a valid city.");
+    })
+
+
 });
 function render() {
-  runningHistory = JSON.parse(localStorage.getItem("#history"))
+  $(".history").empty();
+  runningHistory = JSON.parse(localStorage.getItem("#history")) || [];
   for (let i = 0; i < runningHistory.length; i++) {
-
     const eachCity = runningHistory[i];
     $(".history").append("<p>" + eachCity + "</p>");
   }
